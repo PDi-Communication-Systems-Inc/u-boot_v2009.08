ac565a6ec8d97a1ba61e8774f04d1ac25c39ef1e
Jon Smith <jtsmith@pdiarm.com>
2014-10-15 16:28:33 -0400

    Changed bootloader debug output from UART4 (ttymcx3) to UART1 (ttymcx0)

--- include/configs/mx6q_ar6mx_android-emmc.h
+++ include/configs/mx6q_ar6mx_android-emmc.h
@@ -76,19 +76,19 @@
 		"ethprime=FEC0\0"					\
 		"fastboot_dev=mmc1\0"					\
 		"bootcmd=booti mmc1\0"					\
-		"bootargs=console=ttymxc3,115200 init=/init emmc_en video=mxcfb0:dev=hdmi,1280x720M@60,if=RGB24,bpp=32  video=mxcfb1:off video=mxcfb2:off vmalloc=400M  androidboot.console=ttymxc3 androidboot.hardware=freescale\0"	\
-		"bootargs_base=setenv bootargs console=ttymxc3,115200\0"\
+		"bootargs=console=ttymxc0,115200 init=/init emmc_en video=mxcfb0:dev=hdmi,1280x720M@60,if=RGB24,bpp=32  video=mxcfb1:off video=mxcfb2:off vmalloc=400M  androidboot.console=ttymxc0 androidboot.hardware=freescale\0"	\
+		"bootargs_base=setenv bootargs console=ttymxc0,115200\0"\
 		"bootargs_ldb=setenv bootargs ${bootargs} init=/init emmc_en "	\
 			"video=mxcfb0:dev=ldb,1024x600M@60,if=RGB666,bpp=32 " \
 			"video=mxcfb1:off video=mxcfb2:off vmalloc=400M "	\
-			"androidboot.console=ttymxc3 androidboot.hardware=freescale\0"	\
+			"androidboot.console=ttymxc0 androidboot.hardware=freescale\0"	\
 		"bootargs_hdmi=setenv bootargs ${bootargs} init=/init emmc_en "	\
 			"video=mxcfb0:dev=hdmi,1280x720M@60,if=RGB24,bpp=32 " \
 			"video=mxcfb1:off video=mxcfb2:off vmalloc=400M "	\
-			"androidboot.console=ttymxc3 androidboot.hardware=freescale\0"	\
+			"androidboot.console=ttymxc0 androidboot.hardware=freescale\0"	\
 		"bootargs_dual=setenv bootargs ${bootargs} init=/init emmc_en "	\
 			"video=mxcfb0:dev=ldb,1024x600M@60,bpp=32 " \
 			"video=mxcfb1:dev=hdmi,1280x720M@60,bpp=32 " \
 			"video=mxcfb2:off vmalloc=400M "	\
-			"androidboot.console=ttymxc3 androidboot.hardware=freescale\0"
+			"androidboot.console=ttymxc0 androidboot.hardware=freescale\0"
 #endif
